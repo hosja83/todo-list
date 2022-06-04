412768ba69082600aac6536256d2ae0f03190331
Alhosainy Altaher <alhosainy.altaher@gmail.com>
2022-06-04 19:53:03 -0400

    Created UserException object to handle invalid projectList parameters

---
+++ src/exception.js
@@ -0,0 +1,10 @@
+//exception.js
+
+/**
+ * User-Defined Exception used for identifying type of exception and 
+ * handling errors appropriately
+ */
+export default function UserException(message) {
+  this.message = message;
+  this.name = 'UserException'
+}
