e3dbc52ed2bc8d8cdcf28143350db3f64232f16a
Alhosainy Altaher <alhosainy.altaher@gmail.com>
2022-06-13 23:44:22 -0400

    Added setAttributes and appendChildren methods to DOM library to set many attributes of a single element at once and append many child elements to a parent element at once

--- src/dom-util.js
+++ src/dom-util.js
@@ -22,3 +22,26 @@ export function removeAllChildNodes(parent) {
   while (parent.hasChildNodes()) 
     parent.removeChild(parent.firstChild);
 }
+
+/**
+ * Set the given element's attributes that are given along with their values structured as 
+ * an object with key-value pairs.
+ * 
+ * @param {DOM Element} element DOM element to set attributes
+ * @param {Object} attributes key value pairs containing attributes and their values to set
+ */
+export function setAttributes(element, attributes) {
+  for (const key in attributes)
+    element.setAttribute(key, attributes[key]);
+}
+
+/**
+ * Appends an arbitrary collection of given children that represent HTML Elements to a
+ * parent HTML Element.
+ * 
+ * @param {HTMLElement} parent Parent HTML Element to have children append to
+ * @param {HTMLElement} children Child HTML Elements to be appended to parent
+ */
+export function appendChildren(parent, children) {
+  children.forEach(child => parent.appendChild(child));
+}
