6324e2f9974a5367f8e5336c3b4e8097fa9340fa
Alhosainy Altaher <alhosainy.altaher@gmail.com>
2022-07-15 18:15:26 -0400

    Added gitignore and configured webpack updated

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 //const { dirname } = require('path');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
+  // mode: 'production',
   entry: {
     index: './src/index.js',
   },
@@ -16,7 +17,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Todo List',
-      template: './src/index.html',
+      template: './src/view/index.html',
       filename: 'index.html', //relative to ./dist or root of application
       hash: true,
       //myFirstPageHeader: 'You Can store any variable and call in html file',
@@ -26,20 +27,22 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        include: path.resolve(__dirname, 'src'),
+        include: path.resolve(__dirname, './src/styles'),
         use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.s[ac]ss$/i,
-        include: path.resolve(__dirname, 'src'),
+        include: path.resolve(__dirname, './src/styles'),
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        include: path.resolve(__dirname, './src/assets'),
         type: 'asset/resource',
       },
       {
-       test: /\.(woff|woff2|eot|ttf|otf)$/i, 
+       test: /\.(woff|woff2|eot|ttf|otf)$/i,
+       include: path.resolve(__dirname, './src/assets'),
        type: 'asset/resource',
       },
       {
@@ -54,13 +57,19 @@ module.exports = {
       },
     ],
   },
-  devtool: 'source-map',
+  devtool: 'inline-source-map',
+  // devtool: 'source-map',
+  /* For development only */
+  devServer: {
+    static: './dist',
+  },
   optimization: {
     runtimeChunk: 'single',
   },
-  performance: {
-    hints: false,
-    maxEntrypointSize: 512000,
-    maxAssetSize: 512000
-  },
+  /* Increasing performance during production build mode */
+  // performance: {
+  //   hints: false,
+  //   maxEntrypointSize: 512000,
+  //   maxAssetSize: 512000
+  // },
 };
